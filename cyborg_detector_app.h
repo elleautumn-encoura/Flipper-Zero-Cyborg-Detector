4cea6a9ebe5a918808c20942bbf9297900bcee8c
Elle Autumn <elle.autumn@encoura.org>
2026-01-14 10:13:16 -0600

    Update cyborg_detector_app.h
    Adding feedback for when NFC tag is actively being read ("Cyborg Detected")

--- cyborg_detector_app.h
+++ cyborg_detector_app.h
@@ -3,6 +3,8 @@
 #include <gui/gui.h>
 #include <gui/view_port.h>
 #include <furi_hal_nfc.h>
+#include <notification/notification_messages.h>
+#include <notification/notification_app.h>
 
 typedef struct {
     Gui* gui;
@@ -15,3 +17,17 @@ typedef struct {
 CyborgDetectorApp* cyborg_detector_app_alloc();
 void cyborg_detector_app_free(CyborgDetectorApp* app);
 int32_t cyborg_detector_app(void* p);
+
+typedef struct {
+    Gui* gui;
+    ViewPort* view_port;
+    FuriMessageQueue* event_queue;
+
+    bool running;
+    bool field_active;
+
+    NotificationApp* notifications;
+    bool nfc_active;
+    uint32_t last_activity_tick;
+} CyborgDetectorApp;
+
